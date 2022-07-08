23e80b3ba7a5b617cba1ad46b98c754b877c08d9
xlassix <46370698+xlassix@users.noreply.github.com>
2022-07-08 20:49:21 +0100

    feat: toggle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Flex, Text, Center } from "@chakra-ui/react";
+import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  Dot,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import "./App.css";
+
+const countryData = [
+  {
+    image: "/img/nigeria.png",
+    imageAlt: "nigeria.png",
+    countryName: "NIGERIA",
+  },
+  {
+    image: "/img/cote.png",
+    imageAlt: "cote.png",
+    countryName: "CÔTE D'IVOIRE",
+  },
+  {
+    image: "/img/sn.png",
+    imageAlt: "sen.png",
+    countryName: "SENEGAL",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex
+      className="App"
+      bgColor="rgb(9, 23, 71)"
+      minWidth="100vw"
+      minHeight="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box
+        bgColor="white"
+        margin="auto"
+        padding="1rem"
+        borderRadius="1.5rem"
+        color="rgb(2, 150, 166)"
+      >
+        <img src="/img/logo.gif" alt="logo" className="logo" />
+        <Text textAlign="center" as="h6">
+          SELECT A COUNTRY
+        </Text>
+        <Box>
+          <CarouselProvider
+            naturalSlideWidth={20}
+            naturalSlideHeight={25}
+            visibleSlides={1}
+            totalSlides={countryData.length}
+          >
+            <ButtonBack className="animate">
+              <MdNavigateBefore />
+            </ButtonBack>
+            <Slider>
+              {countryData.map((data, index) => (
+                <Slide index={index}>
+                  <Center>
+                    <img src={data.image} alt={data.imageAlt} />
+                  </Center>
+                  <Text>{data.countryName}</Text>
+                </Slide>
+              ))}
+            </Slider>
+            <ButtonNext className="animate">
+              <MdNavigateNext />
+            </ButtonNext>
+          </CarouselProvider>
+        </Box>
+        <Text textAlign="left">New Client? Click Here</Text>
+        <Text textAlign="left">New Wholesaler? Click Here</Text>
+      </Box>
+    </Flex>
   );
 }
 
